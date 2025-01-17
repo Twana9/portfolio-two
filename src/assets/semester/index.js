1f84689c8623d3f072c6abf0bc2db1964e650902
twana koye <twana.00569020@gmail.com>
2025-01-17 11:44:44 +0300

    pdfs

---
+++ src/assets/semester/index.js
@@ -0,0 +1,20 @@
+import Absorption from "./Absorptionn.pdf";
+import Diffusion from "./diffusion.pdf";
+import Extraction from "./Extraction.pdf";
+import Sa7dan from "./sa7dan.pdf";
+import UnitQuestions from "./UnitQuestions.pdf";
+import Report from "./control system in distillation towers-report.docx";
+
+////Reactor////////////////////////////////////////
+import Sheet1 from "./Sheet 1 answers.pdf";
+import Sheet4 from "./Sheet 4 answers.pdf";
+import ReactorQuestions from "./Reactor questions.pdf";
+
+/////Labs3/////////////////////////////////////////
+import Heat1 from "./heat1.pdf";
+import Service from "./service.docx";
+import Solutions from "./solutions.pdf";
+import BatchReactor from "./batch reactor.pdf";
+
+//////industry/////////////////////////////////////
+import IndustryQuestions from "./questions.pdf";
